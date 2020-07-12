68dcbac557b2bcafb67aadc6681403bb05135436
yurianxdev <bryangarzon22@gmail.com>
2020-07-12 17:23:49 -0500

    New Moderation service added

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,16 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    axios.post("http://localhost:4000/events", event);
+    axios.post("http://localhost:4001/events", event);
+    axios.post("http://localhost:4002/events", event);
 
-    res.send({status: 'OK'});
+    res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-    console.log('Listening on 4005')
+    console.log("Listening on 4005");
 });
